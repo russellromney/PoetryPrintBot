cf772085cb5901bf3f711fb035c5937ff40c8362
Russell Romney <russdude7@gmail.com>
2018-01-05 21:11:02 -0800

    Add files via upload

---
+++ poetry.py
@@ -0,0 +1,20 @@
+import tweepy
+import time
+from poetry import *
+
+poem = 'annabelle_lee.txt'
+
+# authorize the API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+# grab lines from the text file
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
+
+# do the printing
+for line in f:
+    api.update_status(line)
+    time.sleep(900)#Tweet every 15 minutes
